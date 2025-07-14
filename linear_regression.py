6a08ff2cbf3e084268acfd0719814a53433d05f7
al6sK <alexhs246810@gmail.com>
2025-07-14 06:07:18 +0300

    v1.2

--- linear_regression.py
+++ linear_regression.py
@@ -3,12 +3,13 @@ from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 import statsmodels.formula.api as smf
 import warnings
+import numpy as np
 
 warnings.filterwarnings("ignore")
 
 aord= pd.read_csv('data/Asian_Markets/Aord_2025.csv')
 hsi = pd.read_csv('data/Asian_Markets/HSI_2025.csv')
-nikkei = pd.read_csv('data/Asian_Markets/Nikkei_2025.csv').ffill()
+nikkei = pd.read_csv('data/Asian_Markets/Nikkei_2025.csv')
 
 daxi = pd.read_csv('data/EU_Markets/DAX_2025.csv')
 cac40 = pd.read_csv('data/EU_Markets/CAC40_2025.csv')
@@ -63,7 +64,8 @@ plt.close()
 
 formula = 'spy ~ spy_lag1 + sp500 + nasdaq + dji + cac40 + daxi + aord + hsi + nikkei'
 lm = smf.ols(formula=formula, data=Train).fit()
-print(lm.summary())
+
+#print(lm.summary())
 
 Train['PredictedY'] = lm.predict(Train)
 Test['PredictedY'] = lm.predict(Test)
@@ -78,6 +80,7 @@ plt.ylabel('Predicted SPY')
 
 # save plot
 plt.savefig('plots/train_scatter.png', dpi=300)
+plt.close()
 
 # RMSE - Root Mean Squared Error, Adjusted R^2
 def adjustedMetric(data, model, model_k, yname):
@@ -99,4 +102,93 @@ def assessTable(test, train, model, model_k, yname):
     return assessment
 
 print(assessTable(Test , Train , lm , 9 , 'spy').head(2))
+
+#_________________________
+# Strategy Implementation on Train Data
+#_________________________
+Train['Order'] =[1 if sig > 0 else -1 for sig in Train['PredictedY']]
+Train['Profit'] = Train['Order'] * Train['spy']
+# Calculate cumulative wealth
+Train['Wealth'] = Train['Profit'].cumsum()
+print("Total profit made in train: ", Train['Profit'].sum())
+# Calculate Buy and Hold strategy
+Train['BuyHold'] = Train['price'] - Train['price'].iloc[0]
+print("Buy and Hold profit in train: ", Train['price'].iloc[-1] - Train['price'].iloc[0])
+
+
+plt.figure(figsize=(10,10))
+
+plt.plot(Train['Wealth'], label='Strategy Wealth', color='blue')
+plt.plot(Train['BuyHold'], label='Buy & Hold', color='green')
+
+plt.title('Cumulative Returns: Strategy vs Buy & Hold')
+plt.xlabel('Time')
+plt.ylabel('Profit')
+plt.legend()
+plt.grid(True)
+
+plt.savefig('plots/strategy_vs_buyhold_on_Train_Data.png', dpi=300)
+plt.close()
+
+#_________________________
+# Strategy Implementation on Test Data
+#_________________________
+Test['Order'] =[1 if sig > 0 else -1 for sig in Test['PredictedY']]
+Test['Profit'] = Test['Order'] * Test['spy']
+# Calculate cumulative wealth
+Test['Wealth'] = Test['Profit'].cumsum()
+print("Total profit made in Test: ", Test['Profit'].sum())
+# Calculate Buy and Hold strategy
+Test['BuyHold'] = Test['price'] - Test['price'].iloc[0]
+print("Buy and Hold profit in Test: ", Test['price'].iloc[-1] - Test['price'].iloc[0])
+
+
+plt.figure(figsize=(10,10))
+
+plt.plot(Test['Wealth'], label='Strategy Wealth', color='blue')
+plt.plot(Test['BuyHold'], label='Buy & Hold', color='green')
+
+plt.title('Cumulative Returns: Strategy vs Buy & Hold')
+plt.xlabel('Time')
+plt.ylabel('Profit')
+plt.legend()
+plt.grid(True)
+
+plt.savefig('plots/strategy_vs_buyhold_on_Test_Data.png', dpi=300)
+plt.close()
+
+#_________________________
+#sharpe ratio for Train and Test Data
+#_________________________
+Train['Wealth'] = Train['Wealth'] + Train.loc[Train.index[0],'price']
+Test['Wealth'] = Test['Wealth'] + Test.loc[Test.index[0],'price']
+
+Train['Return'] = np.log(Train['Wealth']) - np.log(Train['Wealth'].shift(1))
+dailyr = Train['Return'].dropna()
+
+print("")
+print("daily sharpe ratio on Train data is :" , dailyr.mean() / dailyr.std(ddof=1) ) 
+print("yearly sharpe ratio on Train data is :" , (252**0.5) * dailyr.mean() / dailyr.std(ddof=1) )
+
+Test['Return'] = np.log(Test['Wealth']) - np.log(Test['Wealth'].shift(1))
+dailyr = Test['Return'].dropna()
+
+print("")
+print("daily sharpe ratio on Test data is :" , dailyr.mean() / dailyr.std(ddof=1) ) 
+print("yearly sharpe ratio on Test data is :" , (252**0.5) * dailyr.mean() / dailyr.std(ddof=1) )
+
+#_________________________
+# Maximum drawdown for Train and Test Data
+#_________________________
+
+Train['Peak'] = Train['Wealth'].cummax()
+Train['Drawdown'] = (Train['Peak'] - Train['Wealth']) / Train['Peak']
+
+Test['Peak'] = Test['Wealth'].cummax()
+Test['Drawdown'] = (Test['Peak'] - Test['Wealth'] ) / Test['Peak']
+
+print("")
+print("Maximum drawdonw in train is ", Train['Drawdown'].max())
+print("Maximum drawdonw in test is ", Test['Drawdown'].max())
+
 
